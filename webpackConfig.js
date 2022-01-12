64a3c5541a8e39bbec1d3306b938d8361cfcf1ce
Reem <janinareem@gmail.com>
2022-01-12 18:54:11 +0200

    Add linter files

--- webpackConfig.js
+++ webpackConfig.js
@@ -4,7 +4,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-  devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
