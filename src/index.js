3ff99c8f8cbb105045707ba396ded178457212ac
Reem <janinareem@gmail.com>
2022-01-12 21:49:03 +0200

    Add function in script.js file

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
 import './style.css';
 
-const listContainer = document.querySelector('.list-container');
+// const listContainer = document.querySelector('.list-container');
 
-const task1 = {
-  description: 'Fix my car',
-  completed: true,
-  index: 1,
-};
+// const task1 = {
+//   description: 'Fix my car',
+//   completed: true,
+//   index: 1,
+// };
 
-const task2 = {
-  description: 'Watch series',
-  completed: false,
-  index: 2,
-};
+// const task2 = {
+//   description: 'Watch series',
+//   completed: false,
+//   index: 2,
+// };
 
-const task3 = {
-  description: 'Read articles',
-  completed: false,
-  index: 3,
-};
+// const task3 = {
+//   description: 'Read articles',
+//   completed: false,
+//   index: 3,
+// };
 
-const lists = [task1, task2, task3];
+// const lists = [task1, task2, task3];
 
-function displayTasks(lists) {
-  lists.forEach((list) => {
-    const html = ` <div class"task-div"> <input type="checkbox" name="task" value="task">
-       <label class="list-item" for="task${list.index}"> ${list.description}</label><br>
-      </div>`;
+// function displayTasks(lists) {
+//   lists.forEach((list) => {
+//     const html = ` <div class"task-div"> <input type="checkbox" name="task" value="task">
+//        <label class="list-item" for="task${list.index}"> ${list.description}</label><br>
+//       </div>`;
 
-    listContainer.insertAdjacentHTML('beforeend', html);
-  });
-}
+//     listContainer.insertAdjacentHTML('beforeend', html);
+//   });
+// }
 
-displayTasks(lists);
+// displayTasks(lists);
--- src/script.js
+++ src/script.js
@@ -15,75 +15,91 @@
 
 // })
 
+// const listContainer = document.querySelector('.list-container');
+
+// class List {
+//   lists = [];
+
+//   constructor(description) {
+//     this.desciption = description;
+//   }
+
+//   static setLocalStorage = () => {
+//     const data = JSON.stringify(List.lists);
+//     localStorage.setItem('lists', data);
+//   }
+
+//   // deleteTask(description) {
+//   //   List.lists = List.lists.filter((list) => list.description !== description);
+//   // }
+
+//   createToDoList(list) {
+//     const taskDiv = document.createElement('div');
+//     const description = document.createElement('label');
+//     const checkBox = document.createElement('checkbox');
+//     const dotsIcon = document.createElement('img');
+//     const deletelist = document.createElement('button');
+
+//     taskDiv.appendChild(checkBox);
+//     taskDiv.appendChild(description);
+//     taskDiv.appendChild(dotsIcon);
+//     taskDiv.appendChild(deletelist);
+//     listContainer.appendChild(taskDiv);
+
+//     deletelist.addEventListener('click', () => {
+//       this.deleteTask(list.description);
+//       listContainer.removeChild(taskDiv);
+//     });
+//   }
+
+//   iterateList() {
+//     List.lists.forEach((list) => {
+//       this.createToDoList(list);
+//     });
+//   }
+
+//   saveTasks() {
+//     const list = new List(description.value);
+//     lists.push(list);
+//     List.setLocalStorage(List.lists);
+//     this.createToDoList(list);
+//   }
+// }
+
+
+
+// const list = new List();
+
+import './style.css';
+
 const listContainer = document.querySelector('.list-container');
 
-class List {
-  lists = [];
+const lists = [];
+function displayTasks(lists) {
+  lists.forEach((list) => {
+    const html = ` <div class"task-div"> <input type="checkbox" name="task" value="task">
+       <label class="list-item" for="task${list.index}"> ${list.description}</label><br>
+      </div>`;
 
-  constructor(description) {
-    this.desciption = description;
-  }
-
-  static setLocalStorage = () => {
-    const data = JSON.stringify(List.lists);
-    localStorage.setItem('lists', data);
-  }
-
-  deleteTask(description) {
-    List.lists = List.lists.filter((list) => list.description !== description);
-  }
-
-  createToDoList(list) {
-    const taskDiv = document.createElement('div');
-    const description = document.createElement('label');
-    const checkBox = document.createElement('checkbox');
-    const dotsIcon = document.createElement('img');
-    const deletelist = document.createElement('button');
-
-    taskDiv.appendChild(checkBox);
-    taskDiv.appendChild(description);
-    taskDiv.appendChild(dotsIcon);
-    taskDiv.appendChild(deletelist);
-    listContainer.appendChild(taskDiv);
-
-    deletelist.addEventListener('click', () => {
-      this.deleteTask(list.description);
-      listContainer.removeChild(taskDiv);
-    });
-  }
-
-  iterateList() {
-    List.lists.forEach((list) => {
-      this.createToDoList(list);
-    });
-  }
-
-  saveTasks() {
-    const list = new List(description);
-    lists.push(list);
-    List.setLocalStorage(List.lists);
-    this.createToDoList(list);
-  }
+    listContainer.insertAdjacentHTML('beforeend', html);
+  });
 }
+
 
 const input = document.querySelector('.input-list');
 
-const list = new List();
-
 input.addEventListener('keydown', (e) => {
-  // e.preventDefault();
-  // console.log(e.key);
-
   if (e.key === 'Enter') {
-    saveTasks();
-    description.value = ' ';
+     lists.push(input.value);
+     displayTasks(lists);
+    input.value = ' ';
   }
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-  if (localStorage.getItem('lists') !== null) {
-    const myList = JSON.parse(localStorage.getItem('lists'));
-    List.lists = mylist;
-  }
-  list.iterateList();
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//   if (localStorage.getItem('lists') !== null) {
+//     const myList = JSON.parse(localStorage.getItem('lists'));
+//     List.lists = mylist;
+//   }
+//   list.iterateList();
+// });
