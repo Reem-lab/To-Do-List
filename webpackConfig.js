e7ccbedd696d3d7ffb408282b599dc540deb9646
Reem <janinareem@gmail.com>
2022-01-12 19:01:30 +0200

    fix linter files

--- webpackConfig.js
+++ webpackConfig.js
@@ -1,5 +1,7 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
