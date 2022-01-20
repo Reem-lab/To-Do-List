0fdbe19ca3ee7957d56f06620b9fcdd26ad5daf9
iamgaby <gaby.es.iaa@gmail.com>
2022-01-19 20:34:16 -0500

    Finish - Delete task with id - test

--- remove.js
+++ remove.js
@@ -1,21 +1,17 @@
 const { globaldocument } = require('./jsdom.js');
+
 const del = globaldocument.getElementById('delete');
 
-
 function addDataToLocal(arrayOfTasks) {
-    window.localStorage.setItem('tasks', JSON.stringify(arrayOfTasks));
-  }
-
+  window.localStorage.setItem('tasks', JSON.stringify(arrayOfTasks));
+}
 
 function deleteTaskWith(taskId) {
-    let arrayOfTasks = JSON.parse(window.localStorage.getItem('tasks'));
-    console.log(arrayOfTasks);
-      arrayOfTasks = arrayOfTasks.filter((task) => task.id !== +taskId);
-      console.log(arrayOfTasks);
-    return arrayOfTasks.length;
-    // addDataToLocal(arrayOfTasks);
-  }
-
+  let arrayOfTasks = JSON.parse(window.localStorage.getItem('tasks'));
+  arrayOfTasks = arrayOfTasks.filter((task) => task.id !== +taskId);
+  addDataToLocal(arrayOfTasks);
+  return arrayOfTasks.length;
+}
 
 exports.deleteTaskWith = deleteTaskWith;
-exports.addDataToLocal =addDataToLocal;
+exports.addDataToLocal = addDataToLocal;
--- remove.test.js
+++ remove.test.js
@@ -1,21 +1,18 @@
 const { globaldocument } = require('./jsdom.js');
 const { deleteTaskWith, addDataToLocal } = require('./remove.js');
 
-
 describe('Delete tasks ', () => {
-    test('delete task with id', () => {
-        const newArray = [];
-        globaldocument.getElementById('input-list').value = 'reemgaby';
-        newArray.push({
-          id: Date.now(),
-          title: 'reemgaby',
-          completed: false,
-          index: newArray.length,
-        });
-        addDataToLocal(newArray);
-        console.log(window.localStorage.getItem('tasks'))
-        expect(deleteTaskWith('1642640345257')).toBe(0);
-    })
-
-
-})
+  test('Delete task with id', () => {
+    const newArray = [];
+    globaldocument.getElementById('input-list').value = 'reemgaby';
+    const currentTime = Date.now();
+    newArray.push({
+      id: currentTime,
+      title: 'reemgaby',
+      completed: false,
+      index: newArray.length,
+    });
+    addDataToLocal(newArray);
+    expect(deleteTaskWith(currentTime)).toBe(0);
+  });
+});
